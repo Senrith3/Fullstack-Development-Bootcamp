45021a648d2afb729ca68c0ea9690040969666f4
Senrith3 <ney.senrith19@kit.edu.kh>
2020-09-26 17:25:39 +0700

    update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const taskslist = require('./routes/api/tasklist');
 app.use('/api/taskslist', taskslist);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(_dirname + '/public/'));
+    app.use(express.static(__dirname + '/public/'));
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 const port = process.env.PORT || 5000;
