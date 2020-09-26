22da5dec7920900c187552c7c4c5f22b796cd437
Senrith3 <ney.senrith19@kit.edu.kh>
2020-09-26 13:41:43 +0700

    Added

---
+++ server/routes/api/tasklist.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Tasks
+router.get('/', async (req, res) => {
+    const tasks = await loadTasksCollection();
+    res.send(await tasks.find({}).toArray());
+});
+
+//Add Task
+router.post('/', async (req, res) => {
+    const tasks = await loadTasksCollection();
+    await tasks.insertOne({
+        task: req.body.text,
+        dateCreated: new Date()
+    });
+    res.status(201).send();
+});
+
+//Delete Task
+router.delete('/:id', async (req, res) => {
+    const tasks = await loadTasksCollection();
+    await tasks.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadTasksCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://Senrith:1234@senrith.czumf.gcp.mongodb.net/my_tasklist?retryWrites=true&w=majority', {useNewUrlParser: true});
+    return client.db('my_tasklist').collection('mytasks');
+}
+
+module.exports = router;
